abf13c1452904c345790200e80b706c6a196752e
Rajagopal <rajagopalgms@outlook.com>
2026-01-23 11:44:28 +0530

    set  deployment image to latest

--- my-django-demo/project/views.py
+++ my-django-demo/project/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html", {"message": "Hello from Django on AKS Kudos!"})
+    return render(request, "index.html", {"message": "Hello from Django on AKS Kudos and awesome !"})
 
 def health(request):
     return HttpResponse("OK")
