09af8356a79d22d20cba01167baf1a3ee066ed14
Rajagopal <rajagopalgms@outlook.com>
2026-01-23 08:03:36 +0530

    updated storage for tfs state

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     # Configure these values in your environment or via variables
     resource_group_name  = "tfstate-rg"            # Change as needed
     storage_account_name = "tfstateaccountrajag01" # Change as needed
-    container_name       = "tfstate"               # Change as needed
+    container_name       = "tfstate-aks"               # Change as needed
     key                  = "terraform.tfstate"
   }  
 }
