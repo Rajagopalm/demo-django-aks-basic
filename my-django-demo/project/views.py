7f772b1ab6637654fa291dd9d51560a5db0afe97
Rajagopal <rajagopalgms@outlook.com>
2026-01-23 11:33:16 +0530

    updated welcome message

--- my-django-demo/project/views.py
+++ my-django-demo/project/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html", {"message": "Hello from Django on AKS!"})
+    return render(request, "index.html", {"message": "Hello from Django on AKS Kudos!"})
 
 def health(request):
     return HttpResponse("OK")
