4dd4413753718549d2bcda822541cac0862ca425
Rajagopal <rajagopalgms@outlook.com>
2026-01-23 08:36:32 +0530

    minor fmt fix

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,9 +3,9 @@ terraform {
     # Configure these values in your environment or via variables
     resource_group_name  = "tfstate-rg"            # Change as needed
     storage_account_name = "tfstateaccountrajag01" # Change as needed
-    container_name       = "tfstate-aks"               # Change as needed
+    container_name       = "tfstate-aks"           # Change as needed
     key                  = "terraform.tfstate"
-  }  
+  }
 }
 
 # Replace <your-resource-group> and <yourstorageaccount> with your actual values.
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,41 +1,41 @@
 variable "subscription_id" {
-	type = string
+  type = string
 }
 
 variable "location" {
-	type    = string
-	default = "eastus"
+  type    = string
+  default = "eastus"
 }
 
 variable "rg_name" {
-	type = string
+  type = string
 }
 
 variable "aks_name" {
-	type = string
+  type = string
 }
 
 variable "acr_name" {
-	type = string
+  type = string
 }
 
 variable "dns_zone" {
-	type    = string
-	default = ""
+  type    = string
+  default = ""
 } # optional
 
 variable "node_count" {
-	type    = number
-	default = 2
+  type    = number
+  default = 2
 }
 
 variable "node_size" {
-	type    = string
-	default = "standard_dc2ds_v3"
+  type    = string
+  default = "standard_dc2ds_v3"
 }
 
 variable "github_repo" {
-  type        	= string
-  description 	= "GitHub repository in the format owner/repo for OIDC federation."
-  default 		= "Rajagopalm/demo-django-aks-basic"
+  type        = string
+  description = "GitHub repository in the format owner/repo for OIDC federation."
+  default     = "Rajagopalm/demo-django-aks-basic"
 }
